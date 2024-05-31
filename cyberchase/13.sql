410b8aa6f26637b8d2190a2b4c523582d22c90ea
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-31 00:33:37 -0600

    progress

--- cyberchase/13.sql
+++ cyberchase/13.sql
@@ -3,3 +3,4 @@ write a SQL query to explore a question of your choice. This query should:
 Involve at least one condition, using WHERE with AND or OR
 */
 
+SELECT title, topic FROM episodes WHERE topic LIKE '%Quality%';
