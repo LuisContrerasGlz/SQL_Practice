9b6e618228146c48b10bc599b5b99aa596101287
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-01 14:25:02 -0600

    progress

--- views/8.sql
+++ views/8.sql
@@ -1,4 +1,6 @@
 /*
 
+Write a SQL query to list the English titles of the 5 prints with the least contrast by Hokusai, from least to highest contrast. 
+Compare them to this list on Wikipedia to see if your results match the print’s aesthetics.
 
 */
