c7d4e8e948e09030167ed1b1c84a93a863fc2a41
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-01 14:25:58 -0600

    progress

--- views/10.sql
+++ views/10.sql
@@ -1,4 +1,9 @@
 /*
 
+Write a SQL query to answer a question of your choice about the prints. The query should:
+
+Make use of AS to rename a column
+Involve at least one condition, using WHERE
+Sort by at least one column, using ORDER BY
 
 */
