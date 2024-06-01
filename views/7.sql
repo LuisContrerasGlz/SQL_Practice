16830ff22fd9fd6c14582bfd3a143df60f242d3e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-01 14:24:38 -0600

    progress

--- views/7.sql
+++ views/7.sql
@@ -1,4 +1,6 @@
 /*
 
+Write a SQL query to list the English titles of the 5 brightest prints by Hiroshige, from most to least bright. 
+Compare them to this list on Wikipedia to see if your results match the print’s aesthetics.
 
 */
