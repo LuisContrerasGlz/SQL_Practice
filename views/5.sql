f02cdffdd63c9002d146109448224e37152ccb39
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-01 14:23:47 -0600

    progress

--- views/5.sql
+++ views/5.sql
@@ -1,4 +1,7 @@
 /*
 
+Write a SQL query to find the highest contrast value of prints by Hokusai. 
+Name the column “Maximum Contrast”. 
+Does Hokusai’s prints most contrasting print actually have much contrast?
 
 */
